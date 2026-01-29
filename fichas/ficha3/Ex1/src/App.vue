0f84b28ccd091f663b8378b58aa2be3dfc1eda54
Petúnia Dias <psofia20056@gmail.com>
2026-01-29 16:57:03 +0000

    feat: ficha 3 Ex1

--- fichas/ficha3/Ex1/src/App.vue
+++ fichas/ficha3/Ex1/src/App.vue
@@ -1,11 +1,24 @@
 <template>
-  <div>
-
-  </div>
+  <StudentCard v-for="student in students" :key="student" :name="student.name" :studentNumber="student.studentNumber" :course="student.course" :year="student.year"/>
 </template>
 
 <script>
+  import StudentCard from './components/StudentCard.vue';
+
   export default {
     name: "App",
+    components: {
+      StudentCard
+    },
+
+    data() {
+      return {
+        students: [
+          {name: "Francisco", studentNumber: "324234329", course: "Psicologia", year: 2},
+          {name: "Maria", studentNumber: "329049120", course: "Engenharia Informática", year: 1},
+          {name: "Catarina", studentNumber: "32132109", course: "Ciências Biomédicas", year: 3},
+        ]
+      }
+    }
   }
 </script>
--- fichas/ficha3/Ex1/src/components/StudentCard.vue
+++ fichas/ficha3/Ex1/src/components/StudentCard.vue
@@ -1,15 +1,48 @@
 <template>
     <div>
-
+        <h2>
+            {{ name }}
+        </h2>
+        <p>{{ studentNumber }}</p>
+        <p>{{ course }}</p>
+        <p>{{ year }}</p>
     </div>
 </template>
 
 <script>
     export default {
-        
+        name: "StudentCard",
+        props: {
+            name: {
+                type: String,
+                required: true
+            },
+            studentNumber: {
+                type: Number,
+                required: true,
+                validator(value) {
+                    if (value < 0) {
+                        console.warn(`Invalid student number: ${value}. Must be positive`);
+                        return false;
+                    }
+                    return true;
+                }
+            },
+            course: {
+                type: String,
+                default: "TSIW",
+            },
+            year: {
+                type: Number,
+                default: 2,
+                validator(value) {
+                    if (value < 1 || value > 3) {
+                        console.warn(`Invalid year: ${value}. Must be between 1 and 3`);
+                        return false;
+                    }
+                    return true;
+                }
+            }
+        }
     }
 </script>
-
-<style scoped>
-
-</style>
