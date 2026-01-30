e4a93b7f438845f166078b66b85f4d142bbafb2f
Petúnia Dias <psofia20056@gmail.com>
2026-01-30 14:55:09 +0000

    feat: ficha 3 Ex2 acabado

---
+++ fichas/ficha3/Ex1-2/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <StudentCard v-for="student in students" :key="student" :name="student.name" :studentNumber="student.studentNumber" :course="student.course" :year="student.year"/>
+  <ProductList />
+</template>
+
+<script>
+  import ProductList from './components/ProductList.vue';
+  import StudentCard from './components/StudentCard.vue';
+
+  export default {
+    name: "App",
+    components: {
+      StudentCard,
+      ProductList
+    },
+
+    data() {
+      return {
+        students: [
+          { name: "Francisco", studentNumber: "324234329", course: "Psicologia", year: 2},
+          { name: "Maria", studentNumber: "329049120", course: "Engenharia Informática", year: 1},
+          { name: "Catarina", studentNumber: "32132109", course: "Ciências Biomédicas", year: 3},
+        ],
+
+      }
+    },
+  }
+</script>
---
+++ fichas/ficha3/Ex1-2/src/components/ProductList.vue
@@ -0,0 +1,62 @@
+<template>
+  <ProductCard 
+    v-for="product in products"
+    :key="product.id"
+    :product="product"
+    @add-to-cart="handleAddToCart"
+  />
+</template>
+
+<script>
+import ProductCard from './ProductCard.vue';
+
+    export default {
+        name: "ProductList",
+        components: {
+            ProductCard
+        },
+        data() {
+            return {
+                products: [
+                    {
+                        id: 1,
+                        name: 'Morango',
+                        price: 2.99,
+                        image: 'https://fsp.usp.br/eccco/wp-content/uploads/2022/09/isaac-quesada-DYbr5utVs8s-unsplash-1024x683.jpg',
+                        inStock: true,
+                    },
+                    {
+                        id: 2,
+                        name: 'Banana',
+                        price: 1.49,
+                        image: 'https://images.unsplash.com/photo-1574226516831-e1dff420e42e',
+                        inStock: false,
+                    },
+                    {
+                        id: 3,
+                        name: 'Maçã',
+                        price: 1.99,
+                        image: 'https://images.unsplash.com/photo-1567306226416-28f0efdc88ce',
+                        inStock: true,
+                    },
+                    {
+                        id: 4,
+                        name: 'Abacate',
+                        price: 3.49,
+                        image: 'https://images.unsplash.com/photo-1528825871115-3581a5387919',
+                        inStock: true,
+                    },
+                ]
+            }
+        },
+        methods: {
+            handleAddToCart(productId) {
+                if (!productId) return;
+                const product = this.products.find(p => p.id === productId);
+                if (!product) return;
+                product.inStock = false;
+                alert(`Produto ${product.name} adicionado ao carrinho!`);
+            }
+        }
+    }
+</script>
