2224223c36a2be329d3c2d5810d50594c3c75fe1
Petúnia Dias <psofia20056@gmail.com>
2026-02-04 19:36:49 +0000

    feat: ficha 5 Ex2 inc

--- fichas/ficha5/adminhub/src/router/index.js
+++ fichas/ficha5/adminhub/src/router/index.js
@@ -1,8 +1,51 @@
+import AnalyticsView from '@/views/AnalyticsView.vue'
+import DashboardView from '@/views/DashboardView.vue'
+import LoginView from '@/views/LoginView.vue'
+import ProfileView from '@/views/ProfileView.vue'
+import UnauthorizedView from '@/views/UnauthorizedView.vue'
+import UsersView from '@/views/UsersView.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      name: 'login',
+      component: LoginView,
+      meta: {
+        requiresAuth: false
+      }
+    },
+    {
+      path: '/dashboard',
+      name: 'dashboard',
+      component: () => import('@/views/DashboardView.vue'),
+      meta: {
+        requiresAuth: true
+      }
+    },
+    {
+      path: '/profile',
+      name: 'profile',
+      component: ProfileView
+    },
+    {
+      path: '/users',
+      name: 'users',
+      component: UsersView
+    },
+    {
+      path: '/analytics',
+      name: 'analytics',
+      component: AnalyticsView
+    },
+    {
+      path: '/unauthorized',
+      name: 'anauthorized',
+      component: UnauthorizedView
+    }
+  ],
 })
 
 export default router
